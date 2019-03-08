be29b670ee57172d8b2c6d6a219fe56a963e9d01
Damian Krzeminski <pirxpilot@furkot.com>
2019-03-07 17:21:52 -0700

    emit parsed items one by one
    this is a breaking change - this module used to emit Array of OSM nodes
    the problem with this is that it is easy to run out of memory when
    collecting the Array of OSM items (nodes, ways, or relations)
    
    changed interface will allow implementing reasonable backpressure so
    that we do not emit more nodes that can be processed

--- test/data/generate.js
+++ test/data/generate.js
@@ -5,10 +5,8 @@ var parseOSM = require('../');
 var osm = parseOSM();
 fs.createReadStream(process.argv[2])
     .pipe(osm)
-    .pipe(through.obj(function (items, enc, next) {
-        items.forEach(function (item) {
-            console.log(JSON.stringify(item));
-        });
+    .pipe(through.obj(function (item, enc, next) {
+        console.log(JSON.stringify(item));
         next();
     }))
 ;
